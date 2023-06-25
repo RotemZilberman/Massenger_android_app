cb622b1df1ee2fa94a55cf6a863147800d888313
RotemZilberman <112009232+RotemZilberman@users.noreply.github.com>
2023-06-25 22:39:15 +0300

    Delete backend directory

--- backend/routes/firebaseRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const firebase = require("../controllers/firebaseController.js");
-router.post("/:username", firebase.signUser);
-
-module.exports = router;
